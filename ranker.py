04f18bd63b96668bead3fc426580d80a8c6de1e9
Prajwal Kedilaya <pjkedilaya@gmail.com>
2021-04-15 00:25:21 -0700

    calculate mean returns, stdev, and rank assets by sharpe ratio

--- ranker.py
+++ ranker.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import os
 import glob
+import numpy as np
 
 data_path = os.getcwd()+"/data"
 if os.path.exists(data_path) :   
@@ -9,10 +10,32 @@ else:
     print(f"can't move to {data_path}")
     exit()
 
-file_name_to_csv = {}
+asset_name_to_ret = {}
+asset_names = []
+print("Reading asset prices...")
 for file_name in glob.iglob('*.csv'):
 	n_csv = pd.read_csv(file_name, parse_dates=['Date'])
-	file_name_to_csv[file_name] = n_csv
-	print(f'{file_name} {len(n_csv.index)}')
+	n_prices = n_csv['Adj Close'].values
+	n_returns = n_prices[1:]/n_prices[:-1]-1.
+	asset_name = file_name[:-4]
+	asset_names.append(asset_name)
+	asset_name_to_ret[asset_name] = n_returns
+	print(f'{file_name}\trows: {len(n_csv.index)}'.expandtabs(25))
+print()
 
-# spy = spy_csv['Adj Close'].values
+asset_name_to_mean_ret = {}
+asset_name_to_stdev = {}
+for asset_name in asset_names:
+	a_ret = asset_name_to_ret[asset_name]
+	asset_name_to_mean_ret[asset_name] = np.mean(a_ret)
+	asset_name_to_stdev[asset_name] = np.std(a_ret)
+
+asset_name_to_sr = {}
+for asset_name in asset_names:
+	a_sr = asset_name_to_mean_ret[asset_name]/asset_name_to_stdev[asset_name]
+	asset_name_to_sr[asset_name] = a_sr
+
+sorted_by_sr = dict(sorted(asset_name_to_sr.items(), key=lambda item: item[1]))
+print("Assets ranked by Sharpe Ratio from lowest to highest")
+for asset_name in sorted_by_sr:
+	print(f'{asset_name}\tsharpe ratio: {round(sorted_by_sr[asset_name],3)}'.expandtabs(25))
